efc43b862605601179dfad31a61b8c2710ceaeb7
GunGeekATX <GunGeekATX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-07 15:59:23 +0000

    v1.2.0 updates
    git-svn-id: https://plugins.svn.wordpress.org/any-ipsum/tags/1.2.0@1062329 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/class-WPAnyIpsumCore.php
@@ -0,0 +1,104 @@
+<?php
+/*
+Class: WPAnyIpsumCore
+Author: Pete Nelson (@GunGeekATX)
+
+Exposes filters used by other classes
+
+*/
+
+if (!defined( 'ABSPATH' )) exit('restricted access');
+
+if (!class_exists('WPAnyIpsumCore')) {
+
+	class WPAnyIpsumCore {
+
+		public function plugins_loaded() {
+			add_filter( 'anyipsum-generate-filler', array($this, 'generate_filler') );
+			add_filter( 'anyipsum-parse-request-args', array($this, 'parse_request_args') );
+		}
+
+
+		function parse_request_args($args) {
+
+			$args['type'] = filter_var( $_REQUEST["type"], FILTER_SANITIZE_STRING );
+
+			$number_of_paragraphs = 5;
+			if ( !empty( $_REQUEST["paras"] ) )
+				$number_of_paragraphs = intval( $_REQUEST["paras"] );
+
+			if ($number_of_paragraphs < 1)
+				$number_of_paragraphs = 1;
+
+			if ($number_of_paragraphs > 100)
+				$number_of_paragraphs = 100;
+
+			$args['number-of-paragraphs'] = $number_of_paragraphs;
+			$args['start-with-lorem'] = !empty($_REQUEST["start-with-lorem"]) && '1' === $_REQUEST["start-with-lorem"];
+
+
+			if ( !empty( $_REQUEST["sentences"] ) ) {
+
+				$number_of_sentences = intval( $_REQUEST["sentences"] );
+
+				if ($number_of_sentences < 1)
+					$number_of_sentences = 1;
+
+				if ($number_of_sentences > 100)
+					$number_of_sentences = 100;
+
+				$args['number-of-sentences'] = $number_of_sentences;
+
+			}
+
+
+			return $args;
+
+		}
+
+
+		function generate_filler($args) {
+
+			$args = wp_parse_args( $args, array(
+					'type' => apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'querystring-custom-and-filler' ),
+					'number-of-paragraphs' => 5,
+					'start-with-lorem' => true,
+					'number-of-sentences' => 0,
+				)
+			);
+
+			if (class_exists('WPAnyIpsumGenerator')) {
+
+				$generator = new WPAnyIpsumGenerator();
+				$generator->custom_words 			= $this->get_words( 'custom-words' );
+				$generator->filler 					= $this->get_words( 'filler-words' );
+				$generator->start_with 				= apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'start-with' );
+				$generator->type_all_custom 		= apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'querystring-all-custom' );
+				$generator->type_custom_and_filler 	= apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'querystring-custom-and-filler' );
+				$generator->sentence_mode 			= apply_filters( 'anyipsum-setting-get', false, 'anyipsum-settings-custom-filler', 'sentence-mode' );
+
+				return $generator->Make_Some_Custom_Filler(
+					$args['type'],
+					$args['number-of-paragraphs'],
+					$args['start-with-lorem'],
+					$args['number-of-sentences']
+				);
+
+			}
+			else
+				return array();
+
+		}
+
+
+		function get_words($type) {
+			$words = apply_filters( 'anyipsum-setting-get', array(), 'anyipsum-settings-custom-filler', $type );
+			if (!empty($words))
+				$words = explode("\n", $words);
+			return $words;
+		}
+
+
+	}
+
+}
---
+++ lib/class-WPAnyIpsumForm.php
@@ -0,0 +1,111 @@
+<?php
+/*
+Class: WPAnyIpsumGenerator
+Author: Pete Nelson (@GunGeekATX)
+
+Handles anyipsum-form shortcode
+
+*/
+
+if (!defined( 'ABSPATH' )) exit('restricted access');
+
+if (!class_exists('WPAnyIpsumForm')) {
+
+	class WPAnyIpsumForm {
+
+		public function plugins_loaded() {
+			add_shortcode('anyipsum-form', array($this, 'shortcode_form') );
+		}
+
+
+		function shortcode_form($atts, $content) {
+
+			$output = '';
+
+			$ipsum_name = apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'name' );
+			$start_with = apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'start-with' );
+			$all_custom = apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'querystring-all-custom' );
+			$custom_and_filler = apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'querystring-custom-and-filler' );
+			$all_custom_text = apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'all-custom-text' );
+			$custom_and_filler_text = apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'custom-and-filler-text' );
+			$button_text = apply_filters( 'anyipsum-setting-get', '', 'anyipsum-settings-general', 'button-text' );
+
+			if (empty($ipsum_name))
+				$ipsum_name = 'Lorem';
+
+			if (empty($start_with))
+				$start_with = 'Lorem ipsum dolor amet';
+
+			$type = !empty($_REQUEST['type']) ? $_REQUEST['type'] : $all_custom;
+			$permalink_structure = get_option('permalink_structure');
+
+			ob_start();
+
+			if (!empty($content) && !isset($_REQUEST["type"])) {
+				?>
+					<div class="anyipsum-form-header"><?php echo do_shortcode( $content ); ?>
+				<?php
+			}
+
+
+			?>
+				<form class="anyipsum-form" action="" method="get">
+					<?php if (is_singular() && empty($permalink_structure)) { ?>
+					<input type="hidden" name="p" value="<?php echo esc_attr(get_the_id()); ?>" />
+					<?php } ?>
+					<table class="anyipsum-table">
+						<tbody>
+							<tr class="anyipsum-paragraphs">
+								<td class="anyipsum-left-cell"><?php _e('Paragraphs', 'any-ipsum'); ?>:</td>
+								<td class="anyipsum-right-cell"><input type="text" name="paras" value="5" maxlength="2" /></td>
+							</tr>
+							<tr class="anyipsum-type">
+								<td class="anyipsum-left-cell"><?php _e('Type', 'any-ipsum'); ?>:</td>
+								<td class="anyipsum-right-cell"><label><input type="radio" name="type" value="<?php echo esc_attr($all_custom); ?>" <?php checked($all_custom, $type); ?> /><?php echo esc_attr( $all_custom_text ) ?></label> <label><input type="radio" name="type" value="<?php echo esc_attr($custom_and_filler); ?>" <?php checked($custom_and_filler, $type); ?> /><?php echo esc_attr($custom_and_filler_text); ?></label></td>
+							</tr>
+							<tr class="anyipsum-start-with">
+								<td class="anyipsum-left-cell"></td>
+								<td class="anyipsum-right-cell"><input id="start-with-lorem" type="checkbox" name="start-with-lorem" value="1" checked="checked" /> <label for="start-with-lorem"><?php _e('Start with', 'any-ipsum'); ?> '<?php echo esc_attr($start_with); ?>...'</label></td>
+							</tr>
+							<tr class="anyipsum-submit">
+								<td class="anyipsum-left-cell"></td>
+								<td class="anyipsum-right-cell"><input type="submit" value="<?php echo esc_attr($button_text); ?>" /></td>
+							</tr>
+						</tbody>
+					</table>
+				</form>
+			<?php
+
+			$form = ob_get_contents();
+			ob_end_clean();
+
+			// for customizing/overriding form
+			$form = apply_filters( 'anyipsum-form', $form );
+
+
+			if (isset($_REQUEST["type"])) {
+
+				$args = apply_filters( 'anyipsum-parse-request-args', array() );
+
+				$paragraphs = apply_filters( 'anyipsum-generate-filler', $args );
+
+				$output = '<div class="anyipsum-output">';
+				foreach($paragraphs as $paragraph)
+					$output .= '<p>' . $paragraph . '</p>';
+
+				$output .= '</div>';
+
+				// customize/override output
+				$output = apply_filters( 'anyipsum-form-output', $output, $paragraphs );
+
+			}
+
+			return do_shortcode( $output . $form );
+
+		}
+
+
+	}
+
+}
+
